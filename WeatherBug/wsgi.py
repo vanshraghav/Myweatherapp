e6587c71c9bd6aa0fe1074a0bdf64d75cf1adc60
vanshraghav <114331443+vanshraghav@users.noreply.github.com>
2023-05-28 11:40:09 +0530

    wsgi.py

--- WeatherBug/wsgi.py
+++ WeatherBug/wsgi.py
@@ -15,5 +15,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
 application = get_wsgi_application()
-app = application #add here
+
+app = application 
 
